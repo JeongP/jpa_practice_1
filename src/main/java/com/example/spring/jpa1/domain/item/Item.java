8bbf923f9a247f82e85258ae3faa9436d7931bbe
JeongPyo <jeongpyo_park@tmax.co.kr>
2021-06-18 15:17:15 +0900

    상품 엔티티 개발(비즈니스 로직 추가)

--- src/main/java/com/example/spring/jpa1/domain/item/Item.java
+++ src/main/java/com/example/spring/jpa1/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.example.spring.jpa1.domain.item;
 
 import com.example.spring.jpa1.domain.Category;
+import com.example.spring.jpa1.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,5 +23,28 @@ public abstract class Item {
     private int stockQuantity;
 
     @ManyToMany(mappedBy = "items")
-    private List<Category> categories = new ArrayList<Category>();
+    private List<Category> categories = new ArrayList<>();
+
+    // == 비즈니스 로직 == //
+        // 해당 데이터를 조작하는 비즈니스로직을 도메인 객체 단에서 정의해줌으로써
+        // 응집도를 높일수있음.
+        // setter를 이용하기 보다, 내부의 메서드를 활용해서 값을 컨트롤.
+
+    /**
+     *  stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     *  stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity -= quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
