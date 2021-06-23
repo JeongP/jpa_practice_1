2e2ec405f67d0aaa1ebfc0926ce4e5f57c42ac98
JeongPyo <jeongpyo_park@tmax.co.kr>
2021-06-23 17:25:25 +0900

    order service 개발

--- src/main/java/com/example/spring/jpa1/domain/OrderItem.java
+++ src/main/java/com/example/spring/jpa1/domain/OrderItem.java
@@ -25,6 +25,11 @@ public class OrderItem {
     private int orderPrice; // 주문 시 가격
     private int count; // 주문 수량량
 
+    // 다른 곳에서 생성자 쓰지 못하게 막는 방법.
+    protected OrderItem() {
+
+    }
+
     // == 생성 메서드 == //
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
         OrderItem orderItem = new OrderItem();
